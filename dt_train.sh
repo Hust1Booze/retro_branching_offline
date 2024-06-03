fd664b963185ab5c372d186bb892c921a31ad84b
Hust1Booze <2190288283@qq.com>
2024-06-03 14:28:20 +0800

    忽略dt_out文件夹

--- dt_train.sh
+++ dt_train.sh
@@ -5,7 +5,7 @@ echo "root_dir:"${root_dir}
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # 构建带有日期时间的文件名
-filename="train_dt_${current_time}.txt"
+filename="./dt_out/dt_logs/train_dt_${current_time}.txt"
 
 #python experiments/imitation_trainer.py --config-path=configs --config-name=il.yaml experiment.device=cuda:0 experiment.path_to_save=./model_output experiment.path_to_load_imitation_data=${root_dir}  instances.co_class=set_covering instances.co_class_kwargs.n_rows=165 instances.co_class_kwargs.n_cols=230
 
