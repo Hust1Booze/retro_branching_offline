753aedaba30d3ef20aeba9872947e8d7e13d9b66
Hust1Booze <2190288283@qq.com>
2024-06-07 14:04:14 +0800

    生成500 setcover problem

--- imitation_data_A100.sh
+++ imitation_data_A100.sh
@@ -19,5 +19,5 @@ current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 # 构建带有日期时间的文件名
 filename="il_data__${current_time}.txt"
 
-python experiments/gen_imitation_data.py --config-path=configs --config-name=gen_imitation_data.yaml experiment.path_to_save=${root_dir} instances.co_class=set_covering instances.co_class_kwargs.n_rows=165 instances.co_class_kwargs.n_cols=230
+python experiments/gen_imitation_data.py --config-path=configs --config-name=gen_imitation_data.yaml experiment.path_to_save=${root_dir} instances.co_class=set_covering instances.co_class_kwargs.n_rows=500 instances.co_class_kwargs.n_cols=1000
 
