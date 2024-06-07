a87b5f96e56597729065b274d97b039a87dc731d
Hust1Booze <2190288283@qq.com>
2024-06-08 01:23:37 +0800

    修改il_data节点

--- imitation_data_A100.sh
+++ imitation_data_A100.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -o job.%j.out
-#SBATCH --partition=a100
+#SBATCH --partition=cpu
 #SBATCH -J gen_data
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=2
-#SBATCH --gres=gpu:2
-#SBATCH --qos=a100
+#SBATCH -n 40                 # 总核数 40
+#SBATCH --ntasks-per-node=40   # 每节点核数
+#SBATCH --qos=cpu     
 
 source activate retro_branch_dt
 
