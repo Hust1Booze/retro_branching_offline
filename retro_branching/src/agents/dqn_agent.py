fea33228f22320468c239e3689e93f649877b559
Hust1Booze <2190288283@qq.com>
2024-06-24 14:29:53 +0800

    add top_k 上次缺少提交

--- retro_branching/src/agents/dqn_agent.py
+++ retro_branching/src/agents/dqn_agent.py
@@ -11,7 +11,7 @@ import ml_collections
 import collections.abc
 import numpy as np
 import json
-
+import math
 
 
 
@@ -260,7 +260,7 @@ class DQNAgent:
 
         self.logits = self.calc_Q_values(self.obs, use_target_network=False)
 
-        #self.il_logits = self.action_select_offline(self.obs)
+
 
         if type(self.logits) == list:
             # Q-heads DQN, need to aggregate to get values for each action
@@ -291,16 +291,25 @@ class DQNAgent:
             # batch of observations
             self.preds = self.preds.split_with_sizes(tuple(kwargs['state'].num_candidates))
             self.action_set = kwargs['state'].raw_candidates.split_with_sizes(tuple(kwargs['state'].num_candidates))
-            #print(f'111self.action_set{len(self.action_set)}{self.action_set}\n')
-            #self.sb_action = self.action_set[scores[self.action_set].argmax()]
-            #scores = torch.from_numpy(scores).to(self.device)
-            # for _action_set,score in zip(self.action_set,scores):
-            #     #print(f'_action_set{_action_set}score{score}')
-            #     sb_action_index = torch.from_numpy(score).to(self.device)[_action_set].argmax()
-            #     sb_action = _action_set[sb_action_index]
-            #     print(f'sb_action_index{sb_action_index}sb_action{sb_action}')
             self.sb_action = torch.stack([_action_set[torch.from_numpy(score).to(self.device)[_action_set].argmax()] for _action_set,score in zip(self.action_set,scores)])
-            #print(f'action_set{self.action_set[0]}scores{scores[0]}')
+            
+            # only save top K candidates:
+            K =5
+            total_K_indices = []
+            for i in range(len(self.preds)):
+                # 使用 argsort 返回排序后的索引
+                sorted_indices = np.argsort(scores[i][self.action_set[i].cpu()])
+
+                # 选择最后 5 个元素的索引，即最大的 5 个值
+                top_K_indices = sorted_indices[-K:]
+                # 创建掩码，将所有值初始化为 True
+                mask = torch.ones_like(self.preds[i], dtype=torch.bool)
+
+                # 将最大的 5 个值的位置设置为 False
+                mask[top_K_indices] = False
+                self.preds[i].masked_fill_(mask,-math.inf)
+                total_K_indices.append(top_K_indices)
+
             if kwargs['epsilon'] > 0:
                 # explore
                 raise Exception('Have not yet implemented exploration for batched scenarios.')
@@ -316,35 +325,48 @@ class DQNAgent:
                     self.action_idx = torch.stack([_m.sample() for _m in m]) # sample action from categorical distribution
                 self.action = torch.stack([_action_set[idx] for _action_set, idx in zip(self.action_set, self.action_idx)])
 
-            #print(f'action{self.action.shape}{self.action}\nsb_action{self.sb_action.shape}{self.sb_action}')
-            #print(f'type_preds{type(self.preds)}type_action_set{type(self.action_set)}')
-            
-            final_action = []
-            # policy expansion logic
-            for pred,action_set,action_idx,sb_action in zip(self.preds,self.action_set,self.action_idx,self.sb_action):
-                # get Q-value of normal action and sb action 
-                q_action = pred[action_idx] 
-                sb_action_idx = torch.where(action_set == sb_action)[0].item()
-                q_sb_action = pred[sb_action_idx]
-                
-                # re sample between two actions base one Q-value
-                q = torch.stack([q_action, q_sb_action], dim=-1)
-                logits = q #* self._inv_temperature
-                w_dist = torch.distributions.Categorical(logits=logits)
-                w = w_dist.sample().unsqueeze(-1)   
-                action_idx_temp = (1 - w) * action_idx + w * sb_action_idx
-                # change the final action output
-                action = action_set[action_idx_temp.item()]
-                final_action.append(action)
+                # check action is right
+                for action,score,action_set,_K_indices in zip(self.action,scores,self.action_set,total_K_indices):
+                    assert action.item() in action_set[_K_indices],  "action should in top_k_indices"
 
-            
-            final_action = torch.stack(final_action,dim=0)
-            #print(f'final_action{final_action}')
-            self.action = final_action
+            if False:
+                final_action = []
+                # policy expansion logic
+                for pred,action_set,action_idx,sb_action in zip(self.preds,self.action_set,self.action_idx,self.sb_action):
+                    # get Q-value of normal action and sb action 
+                    q_action = pred[action_idx] 
+                    sb_action_idx = torch.where(action_set == sb_action)[0].item()
+                    q_sb_action = pred[sb_action_idx]
+                    
+                    # re sample between two actions base one Q-value
+                    q = torch.stack([q_action, q_sb_action], dim=-1)
+                    logits = q #* self._inv_temperature
+                    w_dist = torch.distributions.Categorical(logits=logits)
+                    w = w_dist.sample().unsqueeze(-1)   
+                    action_idx_temp = (1 - w) * action_idx + w * sb_action_idx
+                    # change the final action output
+                    action = action_set[action_idx_temp.item()]
+                    final_action.append(action)
+
+                
+                final_action = torch.stack(final_action,dim=0)
+                #print(f'final_action{final_action}')
+                self.action = final_action
 
 
         else:
             # single observation
+            K=5
+            # 使用 argsort 返回排序后的索引
+            sorted_indices = np.argsort(scores[self.action_set])
+            # 选择最后 5 个元素的索引，即最大的 5 个值
+            top_K_indices = sorted_indices[-K:]
+            # 创建掩码，将所有值初始化为 True
+            mask = torch.ones_like(self.preds, dtype=torch.bool)
+
+            # 将最大的 5 个值的位置设置为 False
+            mask[top_K_indices] = False
+            self.preds.masked_fill_(mask,-math.inf)
             if random.random() > kwargs['epsilon']:
                 # exploit
                 if kwargs['munchausen_tau'] == 0 or self.deterministic_mdqn:
@@ -360,37 +382,39 @@ class DQNAgent:
             else:
                 # explore 
                 # uniform random action selection
-                self.action, self.action_idx = self.exploration_agent.action_select(action_set=self.action_set, obs=self.obs, model=kwargs['model'], done=kwargs['done'], sample_stochastically=self.sample_stochastically)
+                self.action, self.action_idx = self.exploration_agent.action_select(action_set=self.action_set[top_K_indices], obs=self.obs, model=kwargs['model'], done=kwargs['done'], sample_stochastically=self.sample_stochastically)
 
-            # policy expansion logic
-            # get Q-value of normal action and sb action 
-            q_action = self.preds[self.action_idx] 
-            sb_action_idx = torch.where(self.action_set == self.sb_action)[0].item()
-            q_sb_action = self.preds[sb_action_idx]
-            
-            # re sample between two actions base one Q-value
-            q = torch.stack([q_action, q_sb_action], dim=-1)
-            logits = q #* self._inv_temperature
-            w_dist = torch.distributions.Categorical(logits=logits)
-            w = w_dist.sample().unsqueeze(-1)   
-            action_idx_temp = (1 - w) * self.action_idx + w * sb_action_idx
-            # change the final action output
-            self.action = self.action_set[action_idx_temp.item()]
-
+            assert self.action.item() in self.action_set[top_K_indices],  "action should in top_k_indices"
             if False:
-                print(f'self.action:{self.action.shape}{self.action}')
-                print(f'self.action_set{self.action_set.shape}{self.action_set}\nscores{len(scores)}{scores}\nself.sb_action{self.sb_action}\n')
-                print(f'self.preds{self.preds.shape}{self.preds}')
-                print(f'self.action_idx{self.action_idx}')
-                print(f'sb_action_idx{sb_action_idx}')
-                print(f'self.action_preds{q_action},self.sb_action_preds{q_sb_action}')
-                print(f'logits{logits}')
-                print(f'w_dist{w_dist}')
-                print(f'w{w}')
-                print(f'action_idx_temp{action_idx_temp}')
+                # policy expansion logic
+                # get Q-value of normal action and sb action 
+                q_action = self.preds[self.action_idx] 
+                sb_action_idx = torch.where(self.action_set == self.sb_action)[0].item()
+                q_sb_action = self.preds[sb_action_idx]
+                
+                # re sample between two actions base one Q-value
+                q = torch.stack([q_action, q_sb_action], dim=-1)
+                logits = q #* self._inv_temperature
+                w_dist = torch.distributions.Categorical(logits=logits)
+                w = w_dist.sample().unsqueeze(-1)   
+                action_idx_temp = (1 - w) * self.action_idx + w * sb_action_idx
+                # change the final action output
+                self.action = self.action_set[action_idx_temp.item()]
 
-            
-            # self.action = self.action_set[self.action_idx.item()]
+                if False:
+                    print(f'self.action:{self.action.shape}{self.action}')
+                    print(f'self.action_set{self.action_set.shape}{self.action_set}\nscores{len(scores)}{scores}\nself.sb_action{self.sb_action}\n')
+                    print(f'self.preds{self.preds.shape}{self.preds}')
+                    print(f'self.action_idx{self.action_idx}')
+                    print(f'sb_action_idx{sb_action_idx}')
+                    print(f'self.action_preds{q_action},self.sb_action_preds{q_sb_action}')
+                    print(f'logits{logits}')
+                    print(f'w_dist{w_dist}')
+                    print(f'w{w}')
+                    print(f'action_idx_temp{action_idx_temp}')
+
+                
+                # self.action = self.action_set[self.action_idx.item()]
 
         # print('final | action set: {} action: {} | action idx: {}'.format(self.action_set, self.action, self.action_idx))
         # if self.profile_time:
