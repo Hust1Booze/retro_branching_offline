c870d76aad4609ba0ad1e8390a28529d36795820
Hust1Booze <2190288283@qq.com>
2024-06-29 14:04:39 +0800

    整理脚本

--- scrips/train.sh
+++ scrips/train.sh
@@ -9,7 +9,5 @@ current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 # 构建带有日期时间的文件名
 filename="validate_${current_time}.txt"
 
-#python experiments/imitation_trainer.py --config-path=configs --config-name=il.yaml experiment.device=cuda:0 experiment.path_to_save=./model_output experiment.path_to_load_imitation_data=${root_dir}  instances.co_class=set_covering instances.co_class_kwargs.n_rows=165 instances.co_class_kwargs.n_cols=230
-
 python experiments/dqn_trainer.py --config-path=configs --config-name=retro_il.yaml experiment.device=cuda:0 learner.path_to_save=${root_dir}/scratch/datasets/offline_retro  instances.co_class=set_covering instances.co_class_kwargs.n_rows=500 instances.co_class_kwargs.n_cols=1000 | tee ${filename}
 
---
+++ scrips/validate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "bash test"
+root_dir=$(pwd)
+echo "root_dir:"${root_dir}
+
+# 使用date命令生成带日期时间的字符串
+current_time=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# 构建带有日期时间的文件名
+filename="validate_${current_time}.txt"
+
+total=/home/liutf/code/retro_branching/scratch/datasets/offline_retro/dqn_learner/dqn_gnn/dqn_gnn_3/
+
+# 遍历文件夹，对每个checkpoint validate
+for item in "$total"/*; do
+  if [ -d "$item" ] && [[ $item == *"checkpoint"* ]]; then  # 如果是目录
+    #echo "Found directory: $item"
+    path="${item}/"
+    #echo ${path}
+    if [ ! -d "$path/rl_validator" ]; then
+	echo "begin validate $path"
+    	python experiments/validator.py --config-path=configs --config-name=validator.yaml environment.observation_function=43_var_and_sb_features  instances.co_class=set_covering instances.co_class_kwargs.n_rows=500 instances.co_class_kwargs.n_cols=1000 experiment.agent_name=offline_retro experiment.path_to_load_agent=${path} experiment.path_to_load_instances=${root_dir}/retro_branching_paper_validation_instances experiment.path_to_save=${path}  | tee "$filename"
+    fi    
+  fi
+done
