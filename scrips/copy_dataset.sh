eb98e4a35df679972e546714aa9dbe93012bcb75
Hust1Booze <2190288283@qq.com>
2024-09-19 00:32:33 +0800

    修改copy脚本

--- scrips/copy_dataset.sh
+++ scrips/copy_dataset.sh
@@ -1,1 +1,3 @@
 scp -r datasets/explore_then_strong_branch liutf@10.16.11.108:/data/ltf/code/retro_branching_offline/datasets/explore_then_strong_branch
+
+scp -r datasets/explore_then_strong_branch_idx liutf@10.16.11.108:/data/ltf/code/retro_branching_offline/datasets/explore_then_strong_branch_idx
